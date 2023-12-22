e006477676d6a310fd59bf08e6089cc7b76e3e03
Firaol Bulo <137378704+firaolkiya@users.noreply.github.com>
2023-12-22 16:16:04 +0300

    Added solution for Merge Strings Alternately

---
+++ pythonTrack/merge-strings-alternately.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        cont = ""
+        i=0
+        while(i<len(word1) or i<len(word2)):
+            if(i<len(word1)):
+                cont+=(word1[i])
+            if(i<len(word2)):
+                cont+=(word2[i])
+            i+=1
+        return cont
