0258889f98d0ef175d8b8fcc5b018a9149508247
Firaol Bulo <137378704+firaolkiya@users.noreply.github.com>
2024-01-04 00:23:25 +0300

    Added solution for Sort the People

---
+++ pythonTrack/sort-the-people.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        for i in range(1,len(heights)):
+            for j in range(i,0,-1):
+                if heights[j]>heights[j-1]:
+                    heights[j],heights[j-1]=heights[j-1],heights[j]
+                    names[j],names[j-1]=names[j-1],names[j]
+                else:
+                    break
+       
+        return names
+
+        
