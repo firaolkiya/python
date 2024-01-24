edcdbb8437d9dd3ff61c2a516f9d608008145032
Firaol Bulo <137378704+firaolkiya@users.noreply.github.com>
2024-01-24 11:19:11 +0300

    Added solution for Sort Colors

--- pythonTrack/sort-colors.py
+++ pythonTrack/sort-colors.py
@@ -1,4 +1,19 @@
-class Solution(object):
-    def sortColors(self, nums):
-        return nums.sort()
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        left=0
+        right=len(nums)-1
+        mid=0
+        while mid<=right:
+            if nums[mid]==0:
+                nums[mid],nums[left]=nums[left],nums[mid]
+                left+=1     
+            elif nums[mid]==2:
+                nums[mid],nums[right]=nums[right],nums[mid]
+                right-=1
+            else:
+                mid+=1
+            if left>mid:
+                mid+=1
+            
+
         
